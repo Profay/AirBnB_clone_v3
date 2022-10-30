5e46b27f0e695eca7c691e24dd93fb7fe5b076a4
PROFAY <85462029+Profay@users.noreply.github.com>
2022-10-30 15:26:18 +0100

    Update 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -9,15 +9,5 @@ $(document).ready(function () {
 	else {
 	    delete checked_box[$(this).data('id')];
 	}
-	$('div.amenities h4').html(function () {
-	    let amenities = [];
-	    Object.keys(checked_box).forEach(function (key) {
-		amenities.push(checked_box[key]);
-	    });
-	    if (amenities.length === 0) {
-		return ('&nbsp');
-	    }
-	    return (amenities.join(', '));
-	});
     });
 });
